01b5830f2f364a6626d7d38ef881b957bb408025
bo-nicolaisen <bni@techcollege.dk>
2025-10-23 08:51:40 +0000

    rth

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,8 +2,9 @@
 let myBigImg=document.getElementById("bigImg")
 
 let smallImgz=document.getElementsByClassName("smallImgz")
-console.log("test")
-smallImgz.array.forEach(element => {
+console.log(smallImgz)
+
+Array.from(smallImgz).forEach(element => {
     console.log(element)
     element.addEventListner("click",(event)=>{
         alert("test")
