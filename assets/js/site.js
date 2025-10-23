e4af1e9a240e69d45a4248b04ce3e29785f7fb60
bo-nicolaisen <bni@techcollege.dk>
2025-10-23 08:45:46 +0000

    test

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,11 @@
 // write cool JS hwere!!
+let myBigImg=document.getElementById("bigImg")
+
+let smallImgz=document.getElementsByClassName("smallImgz")
+console.log("test")
+smallImgz.array.forEach(element => {
+    console.log(element)
+    element.addEventListner("click",(event)=>{
+        alert("test")
+    })
+});
